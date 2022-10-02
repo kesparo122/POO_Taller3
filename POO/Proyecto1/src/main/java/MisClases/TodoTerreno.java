0cc155c431f360aa981de13a31d9cdbf0dd1615e
Santiago <kparrar@unal.edu.co>
2022-10-02 18:36:41 -0500

    subida de archivos 2

--- POO/Proyecto1/src/main/java/MisClases/TodoTerreno.java
+++
@@ -1,7 +0,0 @@
-package MisClases;
-public class TodoTerreno extends Bicicleta{
-    public boolean suspension;
-    public void saltar(){
-        System.out.println("Estoy saltando!");
-    }
-}
--- POO/UIEjemplo/dist/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"uiejemplo","l":"UIEjemplo"},{"p":"uiejemplo","l":"Ventana"}];updateSearchResults();
